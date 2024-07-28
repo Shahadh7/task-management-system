5080c4dbfa2decb209131724f6e598cedf3a3599
shahadh <shahadh.hamza@gmail.com>
2024-07-28 20:41:05 +0530

    Routes has been grouped

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,23 @@ use App\Http\Controllers\StatusController;
 
 Route::middleware('api')->post('/users', [UserController::class, 'store']);
 Route::middleware('api')->post('/login', [UserController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user-protected', [UserController::class, 'protectedRoute']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user-protected', [UserController::class, 'protectedRoute']);
+    Route::get('/statuses',[StatusController::class,'index']);
+    Route::post('/statuses',[StatusController::class,'store']);
+    Route::put('/statuses/{id}',[StatusController::class,'update']);
+    Route::delete('/statuses/{id}',[StatusController::class,'destroy']);
+});
 
 
-Route::middleware('auth:sanctum')->get('/statuses',[StatusController::class,'index']);
-Route::middleware('auth:sanctum')->post('/statuses',[StatusController::class,'store']);
-Route::middleware('auth:sanctum')->put('/statuses/{id}',[StatusController::class,'update']);
-Route::middleware('auth:sanctum')->delete('/statuses/{id}',[StatusController::class,'destroy']);
+
+
+
+
+
+
+
 
 
 
