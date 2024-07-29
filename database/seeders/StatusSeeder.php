f35a1d0e9729425219d5f01d6809b94bf0d6a106
haseefharis <haseefthewind@gmail.com>
2024-07-29 18:27:44 +0530

    seeders file created for Status,Priority

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Status;
+
+class StatusSeeder extends Seeder
+{
+    
+    public function run(): void
+    {
+       $statuses = ['Pending','Ongoing','Completed'];
+       foreach ($statuses as $status){
+            Status::create(['title' => $status]);
+       } 
+    }
+}
